6e5c4f9ec0ae83464262f997370aa2e3b69d11a5
victor <fesenko.victor@gmail.com>
2017-02-10 09:45:11 +0200

    comm

--- run.js
+++ run.js
@@ -3,30 +3,36 @@ const brain = require('brain.js'),
       mnist = require('mnist'),
       softmax = require('./lib/softmax');
 
-var net = new brain.NeuralNetwork();
-
-const set = mnist.set(1, 0, 1);
-
-//const trainingSet = set.training;
-const testSet = set.test;
-
-net.fromJSON(require('./data/brain'));
 
 
-var output = net.run([1,0,1]);
+(async() => {
 
+	// fs.readFile('./data/brain.json', 'utf8', function(err, contents) {
+	//     console.log(contents);
+	// });
 
-var maxRes = 0;
-var res = null;
-for (var i in output){
-	if(output[i]> maxRes){
-		maxRes = output[i]; 
-		res = i;
+	// fs.read(fd, buffer, offset, length, position, callback)
+
+	var net = new brain.NeuralNetwork();
+	const set = mnist.set(1, 0, 1);
+	//const trainingSet = set.training;
+	const testSet = set.test;
+	net.fromJSON(require('./data/brain'));
+	var output = net.run([1,0,1]);
+	var maxRes = 0;
+	var res = null;
+	for (var i in output){
+		if(output[i]> maxRes){
+			maxRes = output[i]; 
+			res = i;
+		}
 	}
-}
+	console.log(testSet[0].output);
+	console.log(maxRes, res);    
+})();
 
 
-console.log(testSet[0].output);
-console.log(maxRes, res);
+
+
 // console.log(softmax(output));
 
